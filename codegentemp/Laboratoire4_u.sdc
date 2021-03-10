1f770312a836d63b75b7b0bacf830446da89d4a6
MathieuChayer <mathieu.chayer@polymtl.ca>
2021-03-10 14:42:16 -0500

    Partie 2.5

--- codegentemp/Laboratoire4_u.sdc
+++ codegentemp/Laboratoire4_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\Users\Mathieu\OneDrive\Desktop\Laboratoire4\Laboratoire4.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\Users\Mathieu\OneDrive\Desktop\Laboratoire4\Laboratoire4.cydsn\Laboratoire4.cyprj
-# Date: Wed, 10 Mar 2021 17:58:41 GMT
+# Date: Wed, 10 Mar 2021 19:29:37 GMT
--- main_cm4.c
+++ main_cm4.c
@@ -16,7 +16,10 @@ Parsa Maghsoudloo (1963539)
 #include "params.h"
 #include "queue.h"
 
+SemaphoreHandle_t bouton_semph;
 
+
+//Partie 1 
 void vLedBlinkTask(void){
     
     for(;;){
@@ -31,11 +34,31 @@ void vLedBlinkTask(void){
     }
 }
 
+//Partie 2 : Interupt pour donner le sémaphore 
+void isr_bouton(void)
+{
+    xSemaphoreGiveFromISR(bouton_semph,NULL);
+    Cy_GPIO_ClearInterrupt(Bouton_0_PORT,Bouton_0_NUM);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+
+}
+
+//Partie 2 : Fonction qui attend le sémaphore
+void bouton_task()
+{
+
+}
+
 int main(void)
 {
+    bouton_semph = xSemaphoreCreateBinary(); //2.6 : Initialisation du sémaphore
 
     __enable_irq(); /* Enable global interrupts. */
     
+    /* Initialisation de l'interuption lié au bouton sw2 */
+    Cy_SysInt_Init(&Bouton_ISR_cfg, isr_bouton);
+    NVIC_ClearPendingIRQ(Bouton_ISR_cfg.intrSrc);
+    NVIC_EnableIRQ(Bouton_ISR_cfg.intrSrc);
     
     xTaskCreate(vLedBlinkTask,"LedBlink",80,NULL,1,NULL);
     vTaskStartScheduler();
